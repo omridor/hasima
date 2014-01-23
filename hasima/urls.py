aa39ba1736aa6074b57d6b69d6ca8f28328f413f
Omri Dor <omridor@gmail.com>
2014-01-23 17:14:54 +0200

    Now with django facebook installed

--- hasima/urls.py
+++ hasima/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^facebook/', include('django_facebook.urls')),
+    url(r'^accounts/', include('django_facebook.auth_urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
